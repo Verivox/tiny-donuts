4532edfe03f088357c20866a23adacd58e273538
Kim Almasan <kim@kumbier.it>
2018-12-04 10:38:34 +0100

    Update build

--- dist/src/Background.d.ts
+++
@@ -1,9 +0,0 @@
-import { Segment } from './Segment';
-export interface IBackgroundParameter {
-    radius: number;
-    size: number;
-}
-export declare class Background extends Segment {
-    constructor();
-    getSegment({ radius, size }: IBackgroundParameter): SVGCircleElement;
-}
--- dist/src/WhiteSegment.d.ts
+++
@@ -1,5 +0,0 @@
-import { Segment } from './Segment';
-export declare class WhiteSegment extends Segment {
-    constructor(size: number);
-    static calculateSize(): void;
-}
